d1135a4558eb31dd2a2c19857de212b74f32544f
Nikhil Pandey <56399423+NikhilPandey1729@users.noreply.github.com>
2022-03-02 12:10:23 +0530

    Create Q2.java

---
+++ core 11/Q2.java
@@ -0,0 +1,17 @@
+package java11;
+
+import java.util.ArrayList;
+class Q2 {
+    public static void main(String[] args)
+    {
+        var<var> al = new ArrayList<>();
+            
+       
+        al.add(60);
+        al.add(70);
+        al.add(30);
+        
+        
+        System.out.println(al);
+    }
+}
