0cba2917dd94867d6a973f3f853060b6d51302cf
Nikhil Pandey <56399423+NikhilPandey1729@users.noreply.github.com>
2022-03-21 02:35:32 +0530

    Add files via upload

---
+++ SpringBoot-HelloWorld/src/main/java/com/java/SpringBootHelloWorld/TestController.java
@@ -0,0 +1,18 @@
+package com.java.SpringBootHelloWorld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+
+
+public class TestController {
+	@RequestMapping("/welcome.html")
+	public ModelAndView firstPage() {
+		return new ModelAndView("Welcome Home");
+	}
+	
+	
+
+}
