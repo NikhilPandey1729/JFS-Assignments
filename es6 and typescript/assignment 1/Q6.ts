b8170f373648871482a38d5bcb45d9e5c15804df
Nikhil Pandey <56399423+NikhilPandey1729@users.noreply.github.com>
2022-04-06 12:05:36 +0530

    Create Q6.ts

---
+++ es6 and typescript/assignment 1/Q6.ts
@@ -0,0 +1,7 @@
+let laptop_model="LENOVO ID 330";
+let desk_no="1111";
+let Customer_name="Nikhil";
+let Ticket=`This Laptop is having a mic issue the model of laptop is: ${laptop_model},
+desk no is:${desk_no},
+Customer_name is:${Customer_name};
+console.log(Ticket);
