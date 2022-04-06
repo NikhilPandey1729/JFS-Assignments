142a014027ca44089bf853292b7af4545e667357
Nikhil Pandey <56399423+NikhilPandey1729@users.noreply.github.com>
2022-04-06 12:08:39 +0530

    Create Q7.ts

---
+++ es6 and typescript/assignment 1/Q7.ts
@@ -0,0 +1,17 @@
+let arr=["Nikhil","arpit","harsh","anupriya"];
+ let[ni, ar, hr, an]=arr;
+ console.log("value of 3rd element is "+hr);
+
+
+ /*b.Create an organization object having 
+ attributes name, address.
+  Write a program to retrieve pin code of an address 
+  using object deep matching.*/
+
+  let pinCode=201204;
+  let organization={
+      name:"NIKHIL",
+      address:"KIRTI Nagar",
+      pinCode
+  }
+  console.log("Pincode is "+pinCode);
