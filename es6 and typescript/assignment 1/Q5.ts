f1257d8c7589885b0f60ec5a0fa5adb80c46bf77
Nikhil Pandey <56399423+NikhilPandey1729@users.noreply.github.com>
2022-04-06 12:04:13 +0530

    Create Q5.ts

---
+++ es6 and typescript/assignment 1/Q5.ts
@@ -0,0 +1,41 @@
+const add=(a=5,b=10)=>
+{
+    console.log(a,b);
+}
+console.log(add());
+
+
+/*b)Write a function userFriends() 
+that takes 2 arguments username & array of user friends. 
+The function should print username & his list of friends. 
+(Use rest parameters)*/
+
+const userFriends=(username: string, ...Hello: string[])=>
+    {
+        console.log(Hello); 
+        console.log(username);
+        for(let i in Hello)
+        {
+            console.log("List of friends Varun has:"+ Hello[i]);
+        }
+
+    }
+let username="userName is:Nikhil";
+userFriends(username,"ayush", "Arpit", "anupriya", "harsh","tyagi");
+
+
+/*c.Write a function printCapitalNames() 
+that takes five names as argument & prints them in capital letters.
+ Use spread operator in order to call printCapitalNames() function.*/
+
+ const printCapitalNames=()=>
+ {
+    for(let i in Names)
+    {
+        console.log("Small list is:"+Names[i]);
+        let a=Names[i].toLocaleUpperCase();
+        console.log("Capital Listis:"+a);
+    }
+ }
+ let Names=['ayush','rajat','lakshay','rahul','Yukta'];
+ printCapitalNames(...Names);
