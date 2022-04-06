fb3e2ebd1f5393c20aa50cf7cf7f7e4edef3dd20
Nikhil Pandey <56399423+NikhilPandey1729@users.noreply.github.com>
2022-04-06 12:11:04 +0530

    Create Q8.ts

---
+++ es6 and typescript/assignment 1/Q8.ts
@@ -0,0 +1,36 @@
+class Account{
+    constructor(id,name,balance){
+        this.id=id;
+        this.name= name;
+        this.balance = balance;
+    }
+}
+class SavingAccount extends Account{
+   constructor(id,name,balance,interest){
+          super(id,name,balance);
+          this.interest  = interest;
+   }
+   getBalance(){
+       this.totalBalance=0;
+       this.totalBalance = this.balance + (this.balance*this.interest);
+       return this.totalBalance;
+   }
+}
+
+class CurrentAccount extends Account{
+   constructor(id,name,balance,cash_credit){
+          super(id,name,balance);
+          this.cash_credit  = cash_credit;
+   }
+   getBalance(){
+       this.totalBalance=0;
+       this.totalBalance = this.balance -this.cash_credit;
+       return this.totalBalance;
+   }
+}
+
+var SavingAccountObj = new SavingAccount(12,"Nikhil",10000,15);
+console.log(SavingAccountObj.getBalance());
+
+var CurrentAccountObj = new CurrentAccount(11,"arpit",70000,200);
+console.log(CurrentAccountObj.getBalance());
